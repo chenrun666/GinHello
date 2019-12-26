ebc456ede3e734463a83f3d5cda8528c99b6b509
chenrun-Mac <17610780919@163.com>
2019-12-26 23:06:11 +0800

    add cookie

--- middleware/Auth.go
+++ middleware/Auth.go
@@ -3,11 +3,20 @@ package middleware
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func Auth() gin.HandlerFunc {
 	return func (context *gin.Context) {
 		fmt.Println("已经授权")
+		cookie, err := context.Request.Cookie("user_cookie")
+		if err == nil {
+			context.SetCookie(cookie.Name, cookie.Value, 1000, cookie.Path, cookie.Domain, cookie.Secure, cookie.HttpOnly)
+			context.Next()
+		} else {
+			context.Abort()
+			context.HTML(http.StatusUnauthorized, "401.tmpl", nil)
+		}
 		context.Next()
 	}
 }
